01e822e40b034b034b0d3e5f10f81dfde9d3ab60
Maolana Alasari <122166967+Maolanaalasari@users.noreply.github.com>
2024-08-07 02:27:28 +0800

    firs commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wallet_ap/src/theme/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'src/pages/homePage.dart';
+import 'src/pages/money_transfer_page.dart';
+import 'src/widgets/customRoute.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Wallet App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme.copyWith(
+          textTheme: GoogleFonts.mulishTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
+        routes: <String, WidgetBuilder>{
+          '/': (_) => HomePage(),
+          '/transfer': (_) => MoneyTransferPage()
+        },
+        // ignore: body_might_complete_normally_nullable
+        onGenerateRoute: (RouteSettings settings) {
+          final List<String> pathElements = settings.name!.split('/');
+          if (pathElements[0].isEmpty) {
+            return null;
+          }
+          if (pathElements[0] == 'transfer') {
+            return CustomRoute<bool>(
+                builder: (BuildContext context) => MoneyTransferPage());
+          }
+        });
+  }
+}
